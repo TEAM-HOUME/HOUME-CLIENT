eaa9db76ea497dd1a7bf43212916d151f0e71a45
Soy <da5868@naver.com>
2025-07-06 21:25:36 +0900

    chore: 리뷰 반영(queryClient 옵션 수정)

--- src/shared/apis/queryClient.ts
+++ src/shared/apis/queryClient.ts
@@ -1,10 +1,16 @@
-import { QueryClient } from '@tanstack/react-query';
+import { QueryCache, QueryClient } from '@tanstack/react-query';
 
 export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      if (import.meta.env.DEV) console.error('[React Query Error]', error);
+    },
+  }),
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false, // 브라우저 포커싱 시 자동 재요청 방지
-      retry: false,
+      retry: 1,
+      staleTime: 1000 * 60 * 5, // 5분
     },
   },
 });
