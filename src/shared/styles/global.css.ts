2f9e031c4c825738785bb25c66ec3db81d77563f
Kyoungho Eom <kheom0512@gmail.com>
2025-07-19 04:21:04 +0900

    style: body 그림자 제거로 시각 효과 수정

--- src/shared/styles/global.css.ts
+++ src/shared/styles/global.css.ts
@@ -98,7 +98,7 @@ globalStyle('body', {
 
   // 시각적 효과
   backgroundColor: colorVars.color.gray000,
-  boxShadow: '0px 0px 30px 0px rgba(0, 0, 0, 0.25)', // 앱 영역 그림자
+  // boxShadow: '0px 0px 30px 0px rgba(0, 0, 0, 0.25)', // 그림자 제거
   scrollBehavior: 'smooth', // 부드러운 스크롤
   scrollbarWidth: 'none', // Firefox 스크롤바 숨김
 });
