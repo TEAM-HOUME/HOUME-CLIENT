3babccb551c8efe078c801f5f425bb9d6eddcdcd
Soy <da5868@naver.com>
2025-07-18 13:37:18 +0900

    feat: 라디오 버튼에 asset추가 및 여백 값 조정

--- src/pages/home/components/introSection/IntroSection.css.ts
+++ src/pages/home/components/introSection/IntroSection.css.ts
@@ -7,7 +7,6 @@ export const wrapper = style({
   flexDirection: 'column',
   alignItems: 'flex-start',
   textAlign: 'center',
-  gap: '1.5rem',
   alignSelf: 'stretch',
 });
 
@@ -31,10 +30,16 @@ export const description = style({
   marginBottom: '4rem',
 });
 
-export const placeholderBox = style({
+export const radioImageBox = style({
   width: '100%',
-  height: '22rem',
-  backgroundColor: colorVars.color.gray100,
+  height: '22.3rem',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
+export const radioImage = style({
+  width: '33.5rem',
   borderRadius: '16px',
 });
 
