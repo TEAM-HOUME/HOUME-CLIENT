faf76d1dd74975fd52475096d0ef64835dd3b305
jstar_00 <wltjd6300@naver.com>
2025-09-16 17:25:11 +0900

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,8 @@ import { RouterProvider } from 'react-router-dom';
 
 import { router } from '@/routes/router';
 
-import ToastTest from './shared/components/toast/ToastTest';
-
 function App() {
-  // return <RouterProvider router={router} />;
-  return <ToastTest />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
