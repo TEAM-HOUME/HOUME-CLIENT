f00f0bdffecd0b9b31bd4da2662336b580f4387f
jstar_00 <wltjd6300@naver.com>
2025-09-16 22:47:33 +0900

    feat: GeneralModal 컴포넌트 스토리북 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,8 @@ import { RouterProvider } from 'react-router-dom';
 
 import { router } from '@/routes/router';
 
-import GeneralModal from './shared/components/overlay/modal/GeneralModal';
-import GeneralModalTest from './shared/components/overlay/modal/GeneralModalTest';
-import OverlayTest from './shared/components/overlay/modal/OverlayTest';
-
 function App() {
-  // return <RouterProvider router={router} />;
-  return <GeneralModalTest />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
