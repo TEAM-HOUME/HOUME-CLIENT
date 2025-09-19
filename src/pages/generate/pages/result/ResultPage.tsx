ac7e5eaf86e3eceeb271ce32ca07b9a991966098
earl9rey <earl9rey@naver.com>
2025-09-19 23:40:49 +0900

    refactor: 이미지 생성 완료 뷰 변경사항 반영

--- src/pages/generate/pages/result/ResultPage.tsx
+++ src/pages/generate/pages/result/ResultPage.tsx
@@ -9,10 +9,8 @@ import CtaButton from '@/shared/components/button/ctaButton/CtaButton';
 import DislikeButton from '@/shared/components/button/likeButton/DislikeButton';
 import LikeButton from '@/shared/components/button/likeButton/LikeButton';
 import Modal from '@/shared/components/overlay/modal/Modal';
-import HeadingText from '@/shared/components/text/HeadingText';
+// import HeadingText from '@/shared/components/text/HeadingText';
 
-import BlurImage from '@assets/icons/recommendBlur.svg?react';
-import LockImage from '@assets/icons/recommendCta.png';
 import Loading from '@components/loading/Loading';
 import {
   useFurnitureLogMutation,
@@ -110,80 +108,47 @@ const ResultPage = () => {
     sendPreference({ imageId: result.imageId, isLike });
   };
 
-  const handleOpenModal = () => {
-    overlay.open(({ unmount }) => (
-      <Modal
-        onClose={unmount}
-        title={`스타일링 이미지대로 가구를\n추천 받으려면 크레딧이 필요해요`}
-        onCreditAction={sendCreditLogs} // 크레딧 액션 콜백 전달
-      />
-    ));
-    sendFurnituresLogs();
-  };
+  // const handleOpenModal = () => {
+  //   overlay.open(({ unmount }) => (
+  //     <Modal
+  //       onClose={unmount}
+  //       title={`스타일링 이미지대로 가구를\n추천 받으려면 크레딧이 필요해요`}
+  //       onCreditAction={sendCreditLogs} // 크레딧 액션 콜백 전달
+  //     />
+  //   ));
+  //   sendFurnituresLogs();
+  // };
 
   // if (isLoading) return <div>로딩중</div>;
   // if (isError || !data) return <div>에러 발생!</div>;
 
-  // 마이페이지에서 온 경우 체크 (이미 위에서 정의했으므로 제거)
-
-  // 조건부 헤더 타이틀 설정
-  const headerTitle = isFromMypage
-    ? '저장된 스타일링 이미지예요!'
-    : '이미지 생성이 완료됐어요!';
-
   return (
     <div className={styles.wrapper}>
-      <section className={styles.headerSection}>
-        {!isFromMypage && <HeadingText title={headerTitle} content="" />}
-        <div className={styles.infoSection}>
-          <p className={styles.infoText}>
-            {`${result.equilibrium}에 거주하며 ${result.tagName}한 취향을 가진\n${result.name}님을 위한 맞춤 인테리어 스타일링이에요!`}
-          </p>
-        </div>
-      </section>
       <section className={styles.resultSection}>
         <img
           src={result.imageUrl}
           alt={`${result.name}님을 위한 맞춤 인테리어 스타일링`}
           className={styles.imgArea({ mirrored: result.isMirror })}
         />
-        <div className={styles.buttonGroup}>
-          <LikeButton
-            size={'large'}
-            onClick={() => handleVote(true)}
-            isSelected={selected === 'like'}
-          >
-            만족스러워요
-          </LikeButton>
-          <DislikeButton
-            size={'large'}
-            onClick={() => handleVote(false)}
-            isSelected={selected === 'dislike'}
-          >
-            아쉬워요
-          </DislikeButton>
-        </div>
-      </section>
-      <section className={styles.curationSection}>
-        <div className={styles.textContainer}>
-          <p className={styles.headerText}>생성한 이미지처럼 방을 꾸며봐요</p>
-          <p className={styles.bodyText}>
-            이미지의 분위기를 기반으로, <br />
-            좋아하실 만한 가구를 골라봤어요!
-          </p>
-        </div>
-        <div className={styles.premiumContentSection}>
-          <BlurImage />
-          <div className={styles.unlockSection}>
-            <img src={LockImage} alt="자물쇠 아이콘" role="presentation" />
-            <CtaButton
-              aria-label="프리미엄 가구 추천 기능 잠금 해제"
-              buttonSize={'small'}
-              fontSize={'body'}
-              onClick={handleOpenModal}
-            >
-              가구 추천받기
-            </CtaButton>
+        <div className={styles.buttonSection}>
+          <div className={styles.buttonBox}>
+            <p className={styles.boxText}>이미지가 마음에 드셨나요?</p>
+            <div className={styles.buttonGroup}>
+              <LikeButton
+                size={'large'}
+                onClick={() => handleVote(true)}
+                isSelected={selected === 'like'}
+              >
+                만족스러워요
+              </LikeButton>
+              <DislikeButton
+                size={'large'}
+                onClick={() => handleVote(false)}
+                isSelected={selected === 'dislike'}
+              >
+                아쉬워요
+              </DislikeButton>
+            </div>
           </div>
         </div>
       </section>
