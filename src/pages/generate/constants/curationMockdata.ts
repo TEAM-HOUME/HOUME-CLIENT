d9b12348ce66b2afa0e64db2450fbfd2a2df53eb
Soy <da5868@naver.com>
2025-09-19 23:28:30 +0900

    add: 필터, 가구 카드 목데이터 추가

---
+++ src/pages/generate/constants/curationMockdata.ts
@@ -0,0 +1,71 @@
+// filter
+export const filterMockData = [
+  { id: 1, furniture: '소파' },
+  { id: 2, furniture: '침대' },
+  { id: 3, furniture: '러그' },
+  { id: 4, furniture: '스탠드' },
+  { id: 5, furniture: '가구' },
+  { id: 5, furniture: '가구' },
+  { id: 5, furniture: '가구' },
+  { id: 5, furniture: '가구' },
+  { id: 5, furniture: '가구' },
+  { id: 5, furniture: '가구' },
+];
+
+// product
+export const productMockData = [
+  {
+    id: 1,
+    title: '제품 이름은 최대 2줄까지 쓸 수 있어요',
+    brand: '브랜드 이름',
+    linkHref: 'https://houme.kr',
+  },
+  {
+    id: 2,
+    title: '편안한 좌식 의자',
+    brand: 'H-Brand',
+    linkHref: 'https://houme.kr',
+  },
+  {
+    id: 3,
+    title: '원목 4인 식탁',
+    brand: 'Wood & Co',
+    linkHref: 'https://houme.kr',
+  },
+  {
+    id: 4,
+    title: '버섯 모양 조명',
+    brand: 'LightLab',
+    linkHref: 'https://houme.kr',
+  },
+  {
+    id: 5,
+    title: '3단 와이드 서랍장',
+    brand: 'StorageX',
+    linkHref: 'https://houme.kr',
+  },
+  {
+    id: 6,
+    title: '러그',
+    brand: 'SoftHome',
+    linkHref: 'https://houme.kr',
+  },
+  {
+    id: 7,
+    title: '미니멀 포스터(액자 포함)',
+    brand: 'Artify',
+    linkHref: 'https://houme.kr',
+  },
+  {
+    id: 8,
+    title: '벽걸이 선반',
+    brand: 'WallUp',
+    linkHref: 'https://houme.kr',
+  },
+  {
+    id: 9,
+    title: '쿠션',
+    brand: 'Cushy',
+    linkHref: 'https://houme.kr',
+  },
+];
