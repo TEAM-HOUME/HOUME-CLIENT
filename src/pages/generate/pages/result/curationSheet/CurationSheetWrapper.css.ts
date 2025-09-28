711ebd6cc7e1b70474a0fedab8352e1c0d1b28ea
Soy <da5868@naver.com>
2025-09-28 19:45:54 +0900

    style: 상단 여백 조절

--- src/pages/generate/pages/result/curationSheet/CurationSheetWrapper.css.ts
+++ src/pages/generate/pages/result/curationSheet/CurationSheetWrapper.css.ts
@@ -17,17 +17,13 @@ export const sheetWrapper = style({
   willChange: 'transform',
   // clamp(최소 상단, 현재 이동값, 최대 하단)
   transform:
-    'translate(-50%, clamp(var(--min-top), calc(var(--base-y) + var(--drag-y)), 100%))',
+    'translate(-50%, clamp(0px, calc(var(--base-y) + var(--drag-y)), 100%))',
   transition: 'transform 300ms ease-in-out',
 
   vars: {
     '--base-y': `calc(100% - ${CURATION_PEEK_HEIGHT})`, // 상태에 따른 목표
     '--drag-y': '0px', // 드래그 변화량
-    '--sheet-max-h': '60rem', // 시트 최대 높이
-
-    // 뷰포트에서 시트를 최대 높이(60rem)까지 올렸을 때
-    // 시트 상단이 화면 위에서 떨어져 있어야 하는 최소 translateY 거리
-    '--min-top': 'max(0px, calc(100vh - var(--sheet-max-h)))',
+    '--sheet-max-h': 'min(var(--sheet-max-h), 100vh)', // 시트 최대 높이
   },
 
   display: 'flex',
@@ -46,8 +42,6 @@ export const snapStyles = styleVariants({
     },
   },
   expanded: {
-    vars: {
-      '--base-y': '0px',
-    },
+    vars: { '--base-y': '0px' },
   },
 });
