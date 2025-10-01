2837c8f0d277feaa56484bad2685313acd43ea0b
문혜성 <coldute1014@naver.com>
2025-10-02 07:02:26 +0900

    feat: card curation 컴포넌트 구현

---
+++ src/pages/mypage/components/card/cardCuration/CardCuration.css.ts
@@ -0,0 +1,26 @@
+import { style } from '@vanilla-extract/css';
+
+export const cardCurationContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '0.6rem',
+  width: '100%',
+});
+
+export const cardImage = style({
+  display: 'flex',
+  width: '16.6rem',
+  height: '16.6rem',
+  justifyContent: 'center',
+  alignItems: 'center',
+  aspectRatio: '1/1',
+  borderRadius: '0.8rem',
+  overflow: 'hidden',
+});
+
+export const image = style({
+  width: '100%',
+  height: '100%',
+  objectFit: 'cover',
+});
