35512e6d46cd131a69ee205acde7671f72454576
earl9rey <earl9rey@naver.com>
2025-10-03 15:56:53 +0900

    feat: 이미지 슬라이드 UI 스타일 추가

--- src/pages/generate/pages/result/components/GeneratedImg.css.ts
+++ src/pages/generate/pages/result/components/GeneratedImg.css.ts
@@ -1,11 +1,16 @@
 import { style } from '@vanilla-extract/css';
 import { recipe } from '@vanilla-extract/recipes';
+
 // import { zIndex } from '@/shared/styles/tokens/zIndex';
+import { fontStyle } from '@/shared/styles/fontStyle';
+
+import { colorVars } from '@styles/tokens/color.css';
 
 export const container = style({
   width: '100%',
   aspectRatio: '3 / 2',
   overflow: 'hidden',
+  position: 'relative',
 });
 
 export const imgArea = recipe({
@@ -27,5 +32,85 @@ export const imgArea = recipe({
   },
   defaultVariants: {
     mirrored: false,
+  },
+});
+
+export const slideNum = style({
+  position: 'absolute',
+  right: '1.2rem',
+  top: '1.2rem',
+  bottom: '22.8rem',
+  width: '3.4rem',
+  height: '2rem',
+  borderRadius: '99.9rem',
+  zIndex: 1,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: colorVars.color.gray000,
+  backgroundColor: colorVars.color.gray999_30,
+  ...fontStyle('caption_r_11'),
+  gap: '0.1rem',
+});
+
+export const slidePrevBtn = style({
+  position: 'absolute',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  left: '1.2rem',
+  bottom: '50%',
+  width: '2.4rem',
+  height: '2.4rem',
+  backgroundColor: colorVars.color.gray999_30,
+  borderRadius: '99.9rem',
+  zIndex: 1,
+
+  ':active': {
+    backgroundColor: colorVars.color.gray999_50,
+  },
+
+  ':disabled': {
+    backgroundColor: colorVars.color.gray000_30,
+  },
+});
+
+export const slideNextBtn = style({
+  position: 'absolute',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  right: '1.2rem',
+  bottom: '50%',
+  width: '2.4rem',
+  height: '2.4rem',
+  backgroundColor: colorVars.color.gray999_30,
+  borderRadius: '99.9rem',
+  zIndex: 1,
+
+  ':active': {
+    backgroundColor: colorVars.color.gray999_50,
+  },
+
+  ':disabled': {
+    backgroundColor: colorVars.color.gray000_30,
+  },
+});
+
+export const tagBtn = style({
+  position: 'absolute',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  right: '1.2rem',
+  bottom: '2.4rem',
+  width: '2.8rem',
+  height: '2.8rem',
+  backgroundColor: colorVars.color.gray999_30,
+  borderRadius: '99.9rem',
+  zIndex: 1,
+
+  ':active': {
+    backgroundColor: colorVars.color.gray999_50,
   },
 });
