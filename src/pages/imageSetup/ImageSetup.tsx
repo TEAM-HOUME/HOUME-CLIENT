faf877389dd5e071cbd691d6137f74a0c7dcf4c7
jstar_00 <wltjd6300@naver.com>
2025-10-09 17:45:57 +0900

    fix: React 버전 업데이트

--- src/pages/imageSetup/ImageSetup.tsx
+++ src/pages/imageSetup/ImageSetup.tsx
@@ -1,3 +1,5 @@
+import { Activity } from 'react';
+
 import FunnelLayout from './components/layout/FunnelLayout';
 import { useImageSetup } from './hooks/useImageGeneration';
 import ActivityInfo from './pages/activityInfo/ActivityInfo';
@@ -22,18 +24,17 @@ export const ImageSetup = () => {
           events: {
             selectHouseInfo: (data: CompletedHouseInfo, { history }) => {
               history.replace('HouseInfo', data);
-              // 각 스텝에서 context에서 사용자 입력값을 가져옴
-              // -> 다음 스텝으로 넘어갈 때 기존 단계에서 선택한 값도 history에 push해야 뒤로가기 시에 데이터가 저장됨
-              // 이때, history.push()는 브라우저 히스토리가 남으므로 history.replace()로 브라우저 히스토리가 남지 않도록 구현
               history.push('FloorPlan', data);
             },
           },
-          render({ dispatch, context }) {
+          render({ dispatch, context, step }) {
             return (
-              <HouseInfo
-                context={context}
-                onNext={(data) => dispatch('selectHouseInfo', data)}
-              />
+              <Activity mode={step === 'HouseInfo' ? 'visible' : 'hidden'}>
+                <HouseInfo
+                  context={context}
+                  onNext={(data) => dispatch('selectHouseInfo', data)}
+                />
+              </Activity>
             );
           },
         })}
@@ -44,12 +45,14 @@ export const ImageSetup = () => {
               history.push('InteriorStyle', data);
             },
           },
-          render({ dispatch, context }) {
+          render({ dispatch, context, step }) {
             return (
-              <FloorPlan
-                context={context}
-                onNext={(data) => dispatch('selectedFloorPlan', data)}
-              />
+              <Activity mode={step === 'FloorPlan' ? 'visible' : 'hidden'}>
+                <FloorPlan
+                  context={context}
+                  onNext={(data) => dispatch('selectedFloorPlan', data)}
+                />
+              </Activity>
             );
           },
         })}
@@ -63,12 +66,14 @@ export const ImageSetup = () => {
               history.push('ActivityInfo', data);
             },
           },
-          render({ dispatch, context }) {
+          render({ dispatch, context, step }) {
             return (
-              <InteriorStyle
-                context={context}
-                onNext={(data) => dispatch('selectInteriorStyle', data)}
-              />
+              <Activity mode={step === 'InteriorStyle' ? 'visible' : 'hidden'}>
+                <InteriorStyle
+                  context={context}
+                  onNext={(data) => dispatch('selectInteriorStyle', data)}
+                />
+              </Activity>
             );
           },
         })}
@@ -81,14 +86,16 @@ export const ImageSetup = () => {
               history.replace('ActivityInfo', data);
             },
           },
-          render({ dispatch, context }) {
+          render({ dispatch, context, step }) {
             return (
-              <ActivityInfo
-                context={context}
-                onContextChange={(data) => dispatch('selectActivityInfo', data)}
-                // 마지막 단계에서는 다음 단계로 넘어가는 버튼이 별도로 존재하지 않으므로,
-                // 버튼을 선택할 때마다 onContextChange로 history에 값을 push하도록 구현
-              />
+              <Activity mode={step === 'ActivityInfo' ? 'visible' : 'hidden'}>
+                <ActivityInfo
+                  context={context}
+                  onContextChange={(data) =>
+                    dispatch('selectActivityInfo', data)
+                  }
+                />
+              </Activity>
             );
           },
         })}
