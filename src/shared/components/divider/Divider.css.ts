30a398bfe2aa33adc8288d626b433d912ae78232
Kyoungho Eom <kheom0512@gmail.com>
2025-10-09 12:36:13 +0900

    fix: Divider 기본 보더 제거

--- src/shared/components/divider/Divider.css.ts
+++ src/shared/components/divider/Divider.css.ts
@@ -5,5 +5,6 @@ import { colorVars } from '@/shared/styles/tokens/color.css';
 export const divider = style({
   width: '100%',
   height: '0.1rem',
+  border: '0',
   backgroundColor: colorVars.color.gray100,
 });
