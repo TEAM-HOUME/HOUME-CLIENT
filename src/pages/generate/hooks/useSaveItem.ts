505f2f1561e6347d84c73bc94e8bf1223be5a0bf
Soy <da5868@naver.com>
2025-10-10 21:32:29 +0200

    chore: 요청 변수 이름 변경

--- src/pages/generate/hooks/useSaveItem.ts
+++ src/pages/generate/hooks/useSaveItem.ts
@@ -12,27 +12,27 @@ export const usePostJjymMutation = () => {
   const toggleSaveProduct = useSavedItemsStore((s) => s.toggleSaveProduct);
 
   return useMutation<SaveItemsResponse, AxiosError, number>({
-    mutationFn: (productId) => postJjym({ recommendFurnitureId: productId }),
+    mutationFn: (recommendFurnitureId) => postJjym({ recommendFurnitureId }),
 
-    onMutate: async (productId) => {
-      toggleSaveProduct(productId);
-      return { productId };
+    onMutate: async (recommendFurnitureId) => {
+      toggleSaveProduct(recommendFurnitureId);
+      return { recommendFurnitureId };
     },
 
-    onSuccess: (data, productId) => {
+    onSuccess: (data, recommendFurnitureId) => {
       const isSavedNow = useSavedItemsStore
         .getState()
-        .savedProductIds.has(productId);
+        .savedProductIds.has(recommendFurnitureId);
 
       // 서버가 돌려준 최종 상태(favorited)와 다르면 한 번 더 토글해서 맞춤
       if (isSavedNow !== data.favorited) {
-        toggleSaveProduct(productId);
+        toggleSaveProduct(recommendFurnitureId);
       }
       console.log('찜하기 성공', data);
     },
 
-    onError: (error, productId) => {
-      toggleSaveProduct(productId); // 롤백
+    onError: (error, recommendFurnitureId) => {
+      toggleSaveProduct(recommendFurnitureId); // 롤백
       console.error('찜하기 토글 변경 중 에러 발생', error);
     },
   });
