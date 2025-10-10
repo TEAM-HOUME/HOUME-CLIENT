bf292c5b6b86150f172d822efdbe4e242f66d355
Soy <da5868@naver.com>
2025-10-10 22:16:10 +0200

    feat: 찜목록 쿼리 키 & 훅 추가

---
+++ src/pages/mypage/hooks/useSaveItemList.ts
@@ -0,0 +1,18 @@
+import { useQuery, type UseQueryOptions } from '@tanstack/react-query';
+
+import { QUERY_KEY } from '@/shared/constants/queryKey';
+
+import { getJjymList } from '../apis/saveItems';
+
+import type { FurnitureItem, JjymsResponse } from '../types/apis/saveItems';
+
+export const useGetJjymList = (
+  options?: UseQueryOptions<JjymsResponse, unknown, FurnitureItem[]>
+) => {
+  return useQuery({
+    queryKey: [QUERY_KEY.JJYM_LIST],
+    queryFn: getJjymList,
+    select: (data) => data.items,
+    ...options,
+  });
+};
