c2c7a81364b3c7eb4ac0780c304b02e78ba95556
Soy <da5868@naver.com>
2025-10-12 22:03:33 +0200

    fix: 토스트 네비게이트시 사라지기 (by.지성)

--- src/shared/components/toast/Toast.tsx
+++ src/shared/components/toast/Toast.tsx
@@ -1,5 +1,7 @@
 import type { JSX } from 'react';
 
+import { type ToastContentProps } from 'react-toastify';
+
 import { TOAST_TYPE, type ToastType } from '@/shared/types/toast';
 
 import WarningIcon from '@assets/icn_warning_toast.svg?react';
@@ -10,6 +12,10 @@ interface ToastProps {
   text: string;
   type: ToastType;
   onClick?: () => void;
+  closeToast?: ToastContentProps['closeToast'];
+  // react-toastify의 ToastContentProps로부터 closeToast 함수 가져옴 -> Toast 컴포넌트에서 closeToast 받아서 사용
+  // 타입은 optional이지만 toast() 함수로 컴포넌트 렌더링 시 react-toastify는 무조건 closeToast 주입(그렇게 설계됨)
+  // +) toast.dismiss(id): 토스트 컴포넌트 외부에서 닫기, closeToast(): 컴포넌트 내부에서 닫기
 }
 
 const ICON_MAP: Record<ToastType, JSX.Element> = {
@@ -18,7 +24,16 @@ const ICON_MAP: Record<ToastType, JSX.Element> = {
   [TOAST_TYPE.WARNING]: <WarningIcon />,
 };
 
-const Toast = ({ text, type, onClick }: ToastProps) => {
+const Toast = ({ text, type, onClick, closeToast }: ToastProps) => {
+  const handleActionClick = () => {
+    if (onClick) {
+      onClick();
+    }
+    if (closeToast) {
+      closeToast();
+    }
+  };
+
   return (
     <div className={styles.container}>
       {ICON_MAP[type]}
@@ -30,7 +45,11 @@ const Toast = ({ text, type, onClick }: ToastProps) => {
         {text}
       </span>
       {onClick && (
-        <button type="button" className={styles.action} onClick={onClick}>
+        <button
+          type="button"
+          className={styles.action}
+          onClick={handleActionClick}
+        >
           보러가기
         </button>
       )}
