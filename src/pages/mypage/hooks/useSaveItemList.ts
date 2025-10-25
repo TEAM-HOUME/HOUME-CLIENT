155239165902fbbaaa1a001e0db5b223ac3afbd1
Soy <da5868@naver.com>
2025-10-25 21:47:56 +0200

    chore: import alias 사용

--- src/pages/mypage/hooks/useSaveItemList.ts
+++ src/pages/mypage/hooks/useSaveItemList.ts
@@ -2,9 +2,12 @@ import { useQuery, type UseQueryOptions } from '@tanstack/react-query';
 
 import { QUERY_KEY } from '@/shared/constants/queryKey';
 
-import { getJjymList } from '../apis/saveItemsList';
+import { getJjymList } from '@pages/mypage/apis/saveItemsList';
 
-import type { FurnitureItem, JjymsResponse } from '../types/apis/saveItemsList';
+import type {
+  FurnitureItem,
+  JjymsResponse,
+} from '@pages/mypage/types/apis/saveItemsList';
 
 export const useGetJjymListQuery = (
   options?: UseQueryOptions<JjymsResponse, unknown, FurnitureItem[]>
