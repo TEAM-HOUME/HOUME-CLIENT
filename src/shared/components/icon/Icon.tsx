f0fef8c24de3afb59a5d1f523146be612651dabb
Soy <da5868@naver.com>
2026-08-18 18:40:24 +0900

    chore: 아이콘 추가

--- src/shared/components/icon/Icon.tsx
+++ src/shared/components/icon/Icon.tsx
@@ -1,3 +1,4 @@
+import ArrowDownFillGreen from '@assets/icons/ArrowDownFillGreen.svg?url';
 import ArrowLeft from '@assets/icons/ArrowLeft.svg?url';
 import ArrowLeftFill from '@assets/icons/ArrowLeftFill.svg?url';
 import ArrowLeftStrokeWhite from '@assets/icons/ArrowLeftStrokeWhite.svg?url';
@@ -55,6 +56,7 @@ import WarningFillDanger from '@assets/icons/WarningFillDanger.svg?url';
 import * as styles from './Icon.css';
 
 const IconsName = {
+  ArrowDownFillGreen,
   ArrowLeft,
   ArrowLeftFill,
   ArrowLeftStrokeWhite,
